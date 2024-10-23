bbb3cc5114672461a58c04c1cde349406f33b070
loicmaes <production@maesloic.fr>
2024-10-23 18:37:10 +0200

    :speech_balloon: Setup authentication literals
    Signed-off-by: loicmaes <production@maesloic.fr>

--- assets/i18n/en.ts
+++ assets/i18n/en.ts
@@ -1,3 +1,35 @@
 export default {
-  helloWorld: "Hello World!",
+  btn: {
+    back: "Back",
+  },
+  auth: {
+    btn: {
+      login: "Join",
+      goToDashboard: "Open app",
+    },
+    register: {
+      title: "Register",
+      fields: {
+        firstName: "First Name",
+        lastName: "Last Name",
+        email: "Email Address",
+        password: "Password",
+        conditions: "I accept the terms of use",
+        promotions: "I accept to receive promotions emails",
+      },
+      action: "Create my account",
+      actionLoading: "Creating your account",
+      link: "I already have an account",
+    },
+    login: {
+      title: "Login",
+      fields: {
+        email: "Email Address",
+        password: "Password",
+      },
+      action: "Take me in",
+      actionLoading: "Logging you in",
+      link: "I don't have an account",
+    },
+  },
 };
