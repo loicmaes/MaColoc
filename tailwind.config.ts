d81e36c696cb33e47ba0df58ea2efdc50ce0ff80
loicmaes <production@maesloic.fr>
2024-10-23 18:32:05 +0200

    :lipstick: Add montserrat font
    Signed-off-by: loicmaes <production@maesloic.fr>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ module.exports = {
   prefix: "",
 
   theme: {
+    fontFamily: {
+      body: "Montserrat, Helvetica, Arial, sans-serif",
+    },
     container: {
       center: true,
       padding: "2rem",
