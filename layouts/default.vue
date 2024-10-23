6b4bfe2477c75f0276f6baec2985580cd1145095
loicmaes <production@maesloic.fr>
2024-10-23 18:37:22 +0200

    :rocket: Update default layout
    Signed-off-by: loicmaes <production@maesloic.fr>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,37 @@
 <script setup lang="ts">
+const user = useUser();
 </script>
 
 <template>
-  <div data-layout="default">
+  <div
+    data-layout="default"
+    class="w-full min-h-dvh"
+  >
+    <header class="sticky top-0 flex items-center justify-between px-6 lg:px-20 py-4">
+      <NuxtLinkLocale
+        class="font-bold"
+        to="/"
+      >
+        Ma Coloc'
+      </NuxtLinkLocale>
+
+      <Button
+        as-child
+      >
+        <NuxtLinkLocale
+          v-if="user"
+          to="/"
+        >
+          {{ $t("auth.btn.goToDashboard") }}
+        </NuxtLinkLocale>
+        <NuxtLinkLocale
+          v-else
+          to="/auth/login"
+        >
+          {{ $t("auth.btn.login") }}
+        </NuxtLinkLocale>
+      </Button>
+    </header>
     <slot />
   </div>
 </template>
