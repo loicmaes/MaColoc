69d395dea4696634b3bc31941cfdbadf560cb681
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-10-31 03:33:01 +0100

    :rocket: Make session persistant when reload

--- app.vue
+++ app.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { IconoirProvider } from "@iconoir/vue";
+import { Toaster } from "~/components/ui/toast";
 
 const iconProps = {
   "width": "1rem",
   "height": "1rem",
   "stroke-width": "2",
 };
+
+await useAuthSession();
 </script>
 
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <Toaster />
     <IconoirProvider :icon-props="iconProps">
       <NuxtLayout>
         <NuxtPage />
