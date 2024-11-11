d5ee1139a57f230b2e8d8dfd7b5045b84b4ce0df
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-11-12 00:49:17 +0100

    :rocket: Add backend route protection

--- server/services/auth.ts
+++ server/services/auth.ts
@@ -7,6 +7,7 @@ import { error, HttpCode } from "~/types/generics/http";
 import * as userRepository from "~/server/database/repositories/user";
 import * as authRepository from "~/server/database/repositories/auth";
 import { readAuthCookies, registerAuthCookies } from "~/server/services/cookies";
+import type { IBackRouteProtection } from "~/types/generics/routeProtection";
 
 export async function loginUser(event: HttpRequest, payload: IAuthLoginBody) {
   try {
@@ -42,4 +43,24 @@ export async function recoverSessionUser(event: HttpRequest) {
   });
 
   return await userRepository.getUser(userUid);
+}
+// eslint-disable-next-line
+export async function useProtectedRoute(event: HttpRequest, callback: (userUid: string) => Promise<any>, options: IBackRouteProtection = { verified: true }) {
+  const { token, userUid } = readAuthCookies(event);
+  if (!token || !userUid) return error(event, {
+    code: HttpCode.Unauthorized,
+    message: "No session provided!",
+  });
+
+  if (!await authRepository.isValid(token, userUid)) return error(event, {
+    code: HttpCode.Unauthorized,
+    message: "Your session has expired!",
+  });
+  if (options.verified && !await userRepository.isVerified(userUid)) return error(event, {
+    code: HttpCode.Forbidden,
+    message: "You're profile is not verified!",
+  });
+  // todo: permissions
+
+  return await callback(userUid);
 }
