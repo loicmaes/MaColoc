92639d744ad0e0ab593fdd005918420fff2c354b
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-11-12 00:50:33 +0100

    :rocket: Update user account created mail template

--- server/email/templates/auth/userAccountCreated.ts
+++ server/email/templates/auth/userAccountCreated.ts
@@ -2,7 +2,7 @@ import type { IMailTemplate } from "~/types/generics/mail";
 
 const useUserAccountCreatedTemplate = (code: string): IMailTemplate => ({
   subject: "Féliciation 🎉 Bienvenue parmi nous !",
-  text: `Ton compte a été créé ! Pense à le vérifier en cliquant sur le lien : http://localhost:3000/auth/verify/${code}`,
+  text: `Ton compte a été créé ! Pense à le vérifier en allant dans tes paramètres et en entrant le code : ${code}`,
   html: "",
 });
 export default useUserAccountCreatedTemplate;
