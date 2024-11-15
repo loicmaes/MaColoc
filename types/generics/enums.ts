dfc533946a3c117ab5b605e186f77273a2283f56
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-11-15 17:26:23 +0100

    :rocket: Add new arrays references to types

--- types/generics/enums.ts
+++ types/generics/enums.ts
@@ -7,3 +7,52 @@ export type TDiet = "vegetarian" | "vegan" | "flexitarian" | "omnivore" | "carni
 export type TDayMoment = "day" | "evening" | "night" | "weekend";
 export type THistoryStatus = "new" | "experienced";
 export type TLanguage = "french" | "english" | "german" | "italian" | "spanish" | "portuguese" | "russian" | "polish" | "indian" | "chinese" | "japanese" | "arabic" | "algerian";
+
+export const maritalStatusRef: TMaritalStatus[] = [
+  "single",
+  "relationship",
+];
+export const frequenciesRef: TFrequency[] = [
+  "regularly",
+  "socially",
+  "never",
+];
+export const dietsRef: TDiet[] = [
+  "vegetarian",
+  "vegan",
+  "flexitarian",
+  "omnivore",
+  "carnivore",
+];
+export const historyStatusRef: THistoryStatus[] = [
+  "new",
+  "experienced",
+];
+export const availabilitiesRef: TDayMoment[] = [
+  "day",
+  "evening",
+  "night",
+  "weekend",
+];
+export const jobStatusRef: TJobStatus[] = [
+  "permanent",
+  "fixedTerm",
+  "student",
+  "selfEmployed",
+  "unemployed",
+];
+export const languagesRef: TLanguage[] = [
+  "french",
+  "english",
+  "german",
+  "italian",
+  "spanish",
+  "portuguese",
+  "russian",
+  "polish",
+  "indian",
+  "chinese",
+  "japanese",
+  "arabic",
+  "algerian",
+];
