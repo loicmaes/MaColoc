d2287f4e5da30a1f074f80f47365d73cb565ada4
loicmaes <production@maesloic.fr>
2024-11-26 16:26:13 +0100

    :rocket: Add new tab system with categories
    Signed-off-by: loicmaes <production@maesloic.fr>

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,33 @@
 import { User } from "@iconoir/vue";
 import UserMenu from "~/components/layouts/user/UserMenu.vue";
 
+const { t } = useI18n();
+
+type NavigationCategory = {
+  label?: string;
+  children: NavigationTab[];
+};
 type NavigationTab = {
   path: string;
   key: string;
 };
-const tabs: NavigationTab[] = [
+const tabs: NavigationCategory[] = [
   {
-    key: "rentalProfile",
-    path: "rentalProfile",
+    children: [
+      {
+        key: "overview",
+        path: "",
+      },
+    ],
+  },
+  {
+    label: "social",
+    children: [
+      {
+        key: "rentalProfile",
+        path: "rentalProfile",
+      },
+    ],
   },
 ];
 </script>
@@ -30,22 +49,32 @@ const tabs: NavigationTab[] = [
       </header>
 
       <nav class="px-1 py-4 flex flex-col">
-        <Button
-          v-for="entry in tabs"
-          :key="entry.key"
-          variant="ghost"
-          class="justify-start"
-          as-child
+        <div
+          v-for="(tab, i) in tabs"
+          :key="tab.label ?? `tab-${i}`"
+          class="flex flex-col"
         >
-          <NuxtLinkLocale
-            :to="`/app/${entry.path}/`"
-            active-class="bg-secondary"
-            exact-active-class="bg-secondary"
+          <span
+            v-if="tab.label"
+            class="ml-3 mb-1 mt-4 font-medium text-sm text-muted-foreground"
+          >{{ t(`nav.sections.${tab.label}`) }}</span>
+          <Button
+            v-for="entry in tab.children"
+            :key="entry.key"
+            variant="ghost"
+            class="justify-start"
+            as-child
           >
-            <User />
-            <span>{{ $t(`${entry.key}.navigationLabel`) }}</span>
-          </NuxtLinkLocale>
-        </Button>
+            <NuxtLinkLocale
+              :to="`/app/${entry.path}/`"
+              active-class="bg-secondary"
+              exact-active-class="bg-secondary"
+            >
+              <User />
+              <span>{{ t(`${entry.key}.navigationLabel`) }}</span>
+            </NuxtLinkLocale>
+          </Button>
+        </div>
       </nav>
     </aside>
 
