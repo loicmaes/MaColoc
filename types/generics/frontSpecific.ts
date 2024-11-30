2d0a228c26ce98bced9231bce99031c3517dad79
loicmaes <production@maesloic.fr>
2024-11-30 03:00:35 +0100

    :rocket: Add navigation tab typing
    Signed-off-by: loicmaes <production@maesloic.fr>

--- types/generics/frontSpecific.ts
+++ types/generics/frontSpecific.ts
@@ -2,3 +2,15 @@ import type { ComposerTranslation, DefineLocaleMessage } from "vue-i18n";
 import type { RemoveIndexSignature } from "@intlify/core-base";
 
 export type InternalizationTool = ComposerTranslation<RemoveIndexSignature<{ [p: string]: DefineLocaleMessage[string] }>>;
+
+export interface NavigationCategory {
+  label?: string;
+  prefix?: string;
+  children: NavigationTab[];
+}
+export interface NavigationTab {
+  key: string;
+  path: string;
+  icon?: Component;
+  renderCondition?: boolean;
+}
