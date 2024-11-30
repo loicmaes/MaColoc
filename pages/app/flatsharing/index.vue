22a6f7c40a55a5460841dc2d1b7107a562c3082b
loicmaes <production@maesloic.fr>
2024-11-30 10:15:36 +0100

    :fire: Remove empty statement
    Signed-off-by: loicmaes <production@maesloic.fr>

--- pages/app/flatsharing/index.vue
+++ pages/app/flatsharing/index.vue
@@ -17,23 +17,7 @@ const user = useUser();
     data-page="flatsharing-home"
     class="flex flex-col"
   >
-    <template v-if="false">
-      <!-- TODO -->
-      todo: display flat sharing space
-    </template>
-    <template v-else>
-      <div class="py-12 flex flex-col items-center gap-2 max-w-[35ch] mx-auto">
-        <h1 class="text-2xl text-center font-bold text-pretty">
-          {{ t("flatSharing.home.emptyStatus.title") }}
-        </h1>
-        <p class="text-center text-muted-foreground">
-          {{ t("flatSharing.home.emptyStatus.caption") }}
-        </p>
-        <Button class="mt-6">
-          <Plus />
-          <span>{{ t("flatSharing.home.emptyStatus.action") }}</span>
-        </Button>
-      </div>
-    </template>
+    <!-- TODO -->
+    todo: display flat sharing space
   </div>
 </template>
