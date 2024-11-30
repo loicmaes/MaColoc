fbb9b8be1dbc10e16871b72b21691f5425fd2bd9
loicmaes <production@maesloic.fr>
2024-11-30 03:01:04 +0100

    :rocket: Add default flat sharing pages
    Signed-off-by: loicmaes <production@maesloic.fr>

---
+++ pages/app/flatsharing/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Plus } from "@iconoir/vue";
+import CreateFlatSharingSpace from "~/components/composed/flatsharing/CreateFlatSharingSpace.vue";
+
+const { t } = useI18n();
+const user = useUser();
+</script>
+
+<template>
+  <div
+    v-if="user"
+    data-page="flatsharing-home"
+    class="flex flex-col"
+  >
+    <template v-if="false">
+      <!-- TODO -->
+      todo: display flat sharing space
+    </template>
+    <template v-else>
+      <div class="py-12 flex flex-col items-center gap-2 max-w-[35ch] mx-auto">
+        <h1 class="text-2xl text-center font-bold text-pretty">
+          {{ t("flatSharing.home.emptyStatus.title") }}
+        </h1>
+        <p class="text-center text-muted-foreground">
+          {{ t("flatSharing.home.emptyStatus.caption") }}
+        </p>
+        <Button class="mt-6">
+          <Plus />
+          <span>{{ t("flatSharing.home.emptyStatus.action") }}</span>
+        </Button>
+      </div>
+    </template>
+  </div>
+</template>
---
+++ pages/app/flatsharing/members.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div data-page="flatsharing-members">
+    <h1>members</h1>
+  </div>
+</template>
